7d879cd2e1e4155f1cd0109e55eb2f8a97374b2f
leeheeweon <ggoddii@kaoni.com>
2023-09-19 17:05:29 +0900

    jpa 매핑

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Main.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
