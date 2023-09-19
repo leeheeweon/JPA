7dc9f006031d8104addc244308fa47a970720d1d
leeheeweon <ggoddii@kaoni.com>
2023-09-19 16:30:20 +0900

    JPA 기기초

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDate;
 import java.util.List;
 
 public class JpaMain {
@@ -13,14 +14,14 @@ public class JpaMain {
 
         EntityTransaction tx = em.getTransaction();
         tx.begin();
-        try {
-//            Member member = em.find(Member.class, 1L);
-            List<Member> result = em.createQuery("select m from Member as m",Member.class)
-                    .getResultList();
 
-            for (Member member: result) {
-                System.out.println("member.name= " + member.getName());
-            }
+        try {
+            //영속
+            Member member = new Member();
+            member.setUsername("lgu+");
+
+            em.persist(member);
+
 
             tx.commit();
         } catch (Exception e) {
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,14 +1,19 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Date;
 
 @Entity
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
-    private String name;
+    private String username;
+
+    public Member() {
+    }
 
     public Long getId() {
         return id;
@@ -18,11 +23,11 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
