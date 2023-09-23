7843bbac4f97ace86512c3f3631ebc4db38f13a7
leeheeweon <ggoddii@kaoni.com>
2023-09-23 12:00:00 +0900

    기본값 타입

---
+++ ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
@@ -0,0 +1,14 @@
+package hellojpa;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class ValueMain {
+
+    public static void main(String[] args) {
+        Integer a = new Integer(10);
+        Integer b = a;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
