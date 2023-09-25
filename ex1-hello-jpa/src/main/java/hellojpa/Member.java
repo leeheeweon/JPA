f88101ea95b60bae6ab4502a382d92be81caaca6
leeheeweon <ggoddii@kaoni.com>
2023-09-25 10:37:46 +0900

    member

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -3,10 +3,10 @@ package hellojpa;
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
+import java.util.*;
 
 @Entity
-@SequenceGenerator(name = "MEMBER_SEQ_GEN",sequenceName = "MEMBER_SEQ")
+@SequenceGenerator(name = "MEMBER_SEQ_GEN", sequenceName = "MEMBER_SEQ")
 public class Member {
 
     @Id
@@ -25,6 +25,15 @@ public class Member {
 
     @Embedded
     private Address homeAddress;
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name = "ADDRESS", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    private List<Address> addressesHistory = new ArrayList<>();
 
     public Long getId() {
         return id;
