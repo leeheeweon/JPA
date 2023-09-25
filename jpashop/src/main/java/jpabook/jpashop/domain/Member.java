b8a05fb318d6fb345dec5fed7e749d3bef31ea98
leeheeweon <ggoddii@kaoni.com>
2023-09-25 10:44:53 +0900

    값타ㅏ입

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -13,11 +13,12 @@ public class Member {
     @Column(name = "MEMBER_ID")
     private Long id;
     private String name;
-    private String city;
-    private String street;
-    private String zipcode;
+
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>(); //관례상 넣어줄것...
+
+    @Embedded
+    private Address address;
 
     public Long getId() {
         return id;
@@ -35,35 +36,19 @@ public class Member {
         this.name = name;
     }
 
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
     public List<Order> getOrders() {
         return orders;
     }
 
     public void setOrders(List<Order> orders) {
         this.orders = orders;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
     }
 }
