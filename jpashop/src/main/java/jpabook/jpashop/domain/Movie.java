9d72ca7e50103f98bba4ab4d417a8952ae0b566a
leeheeweon <ggoddii@kaoni.com>
2023-09-25 09:20:05 +0900

    new file

--- jpashop/src/main/java/jpabook/jpashop/domain/Movie.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Movie.java
@@ -1,2 +1,27 @@
-package jpabook.jpashop.domain;public class Movie {
+package jpabook.jpashop.domain;
+
+import javax.persistence.Entity;
+import javax.xml.soap.SAAJMetaFactory;
+
+@Entity
+public class Movie extends Item {
+
+    private String director;
+    private String actor;
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getActor() {
+        return actor;
+    }
+
+    public void setActor(String actor) {
+        this.actor = actor;
+    }
 }
