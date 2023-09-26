d978cc16bbb2d9699bd1d34750185ce6c89d70e3
leeheeweon <ggoddii@kaoni.com>
2023-09-26 09:38:38 +0900

    jpql 기본문법

---
+++ jpql/src/main/java/JpaMain.java
@@ -0,0 +1,33 @@
+import jpql.Member;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setUsername("leeheeweon");
+            member.setAge(10);
+
+            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
+            Member singleResult = query.getSingleResult();
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+
+}
