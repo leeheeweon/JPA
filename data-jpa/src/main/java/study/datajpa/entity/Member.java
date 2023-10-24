c63807d6cf33b376a09b2cef1b397045d5dfb6a1
leeheeweon <ggoddii@kaoni.com>
2023-10-24 15:22:10 +0900

    도메인 클래스 컨버터

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -8,7 +8,7 @@ import lombok.*;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of = {"id","username","age"})
-public class Member {
+public class Member extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "member_id", nullable = false)
