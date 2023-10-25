3f77e21b4576aaaffe567137061cf9b00950d2e1
leeheeweon <ggoddii@kaoni.com>
2023-10-25 16:14:27 +0900

    기본 q타입 활용

--- querydsl/src/test/java/study/querydsl/entity/MemberTest.java
+++ querydsl/src/test/java/study/querydsl/entity/MemberTest.java
@@ -4,7 +4,6 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -16,7 +15,6 @@ class MemberTest {
     EntityManager em;
 
     @Test
-    @Rollback(value = false)
     public void testEntity() {
         Team teamA = new Team("teamA");
         Team teamB = new Team("teamB");
