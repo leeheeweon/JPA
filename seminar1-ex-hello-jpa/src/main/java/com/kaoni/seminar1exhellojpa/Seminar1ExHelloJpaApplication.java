25a888bfe293992e558ab4b70ca41ef9bbc4acf8
leeheeweon <ggoddii@kaoni.com>
2023-11-10 10:40:24 +0900

    maven 설정 추가

---
+++ seminar1-ex-hello-jpa/src/main/java/com/kaoni/seminar1exhellojpa/Seminar1ExHelloJpaApplication.java
@@ -0,0 +1,13 @@
+package com.kaoni.seminar1exhellojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seminar1ExHelloJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seminar1ExHelloJpaApplication.class, args);
+	}
+
+}
---
+++ seminar1-ex-hello-jpa/src/test/java/com/kaoni/seminar1exhellojpa/Seminar1ExHelloJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaoni.seminar1exhellojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seminar1ExHelloJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
